7bb15af15ff62eeee398fbfd9fa9ff501886ed75
P.Mahesh <46820201+mahesh122000@users.noreply.github.com>
2019-12-04 13:25:01 +0530

    Update twitter_client.py

--- twitter_client.py
+++ twitter_client.py
@@ -6,10 +6,10 @@ from tweepy import OAuthHandler
 def get_twitter_auth():
     
     try:
-        consumer_key="RC2Kl8eVrzz1wEd7vIJLKbPWX"
-        consumer_secret="9TC1hx6wdJLvfss5AW8NF8I6TmIuchHS9rXmt2752iaStKSHeH"
-        access_token="956451552633397248-Xc3E5KMwUOBe7I2fHisHLYUnnDmiFuS"
-        access_secret="7o7xZmNobDfeNWq0x8PSlIOwvyWpRQJSsZtAzZKAO99Hl"
+        consumer_key="XXXXX"
+        consumer_secret="XXXXX"
+        access_token="XXXXX"
+        access_secret="XXXXXX"
     except KeyError:
         print("error in twitter key's ...")
     auth=OAuthHandler(consumer_key,consumer_secret)
